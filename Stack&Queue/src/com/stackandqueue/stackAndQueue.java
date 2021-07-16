31441909c63518f12f9cf4f472a54272c848a5e2
Vivek varshney <Vivek.iitd93@gmail.com>
2021-07-16 18:22:27 +0530

    Welcome to the Stack And Queue Problem

---
+++ Stack&Queue/src/com/stackandqueue/stackAndQueue.java
@@ -0,0 +1,7 @@
+package com.stackandqueue;
+
+public class stackAndQueue {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Stack And Queue Problem");
+	}
+}
---
+++ Stack&Queue/src/module-info.java
@@ -0,0 +1,2 @@
+module stackAndQueue {
+}
